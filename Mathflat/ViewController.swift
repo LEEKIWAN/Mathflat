65424329503d3779c122978acb9f6859d10e0a15
kiwan <kiwan0930@gmail.com>
2021-04-26 23:59:06 +0900

    - init

--- Mathflat/ViewController.swift
+++ Mathflat/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import Sketch
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sketchView: SketchView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - Event
+    
+    @IBAction func onSaveTouched(_ sender: UIButton) {
+        guard let sketchView = sketchView else { return }
+        let image = UIGraphicsImageRenderer(bounds: sketchView.bounds).image { _ in
+            sketchView.drawHierarchy(in: sketchView.bounds, afterScreenUpdates: true)
+        }
+        
+        if let data = image.pngData() {
+            UserDefaults.standard.set(data, forKey: "snapshot")
+        }
+        
+    }
+    
+    @IBAction func onLoadTouched(_ sender: UIButton) {
+        guard let data = UserDefaults.standard.data(forKey: "snapshot"), let image = UIImage(data: data) else { return }
+        guard let resizedImage = ImageResizer.shared.resize(image: image, targetSize: sketchView.bounds.size) else { return }
+        sketchView.loadImage(image: resizedImage)
+    }
+    
+    @IBAction func onAddPictureTouched(_ sender: UIButton) {
+        PhotoRequestManager.requestPhotoFromSavedPhotosAlbum(self){ [weak self] result in
+            switch result {
+            case .success(let image):
+                if let sketchView = self?.sketchView, let resizedImage = ImageResizer.shared.resize(image: image, targetSize: sketchView.bounds.size) {
+                    
+                    self?.sketchView.loadImage(image: resizedImage)
+                }
+            case .faild:
+                print("failed")
+            case .cancel:
+                break
+            }
+        }
+    }
+    
+    
+    @IBAction func onUndoTouched(_ sender: UIButton) {
+        sketchView.undo()
+    }
+    
+    @IBAction func onRedoTouched(_ sender: UIButton) {
+        sketchView.redo()
+    }
+    
+    @IBAction func onPenTouched(_ sender: UIButton) {
+        sketchView.drawTool = .pen
+    }
+    
+    @IBAction func onEraseTouched(_ sender: UIButton) {
+        sketchView.drawTool = .eraser
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Mathflat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Mathflat
+  pod 'Sketch'
+
+end

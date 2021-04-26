1e8334c13fe1e7d3122eaac37d789a9cbb16865f
kiwan <kiwan0930@gmail.com>
2021-04-26 22:09:48 +0900

    Initial Commit

---
+++ Mathflat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mathflat
+//
+//  Created by kiwan on 2021/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
